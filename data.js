7cb5177fb2e4823a423beefdfdc89f4222abfb34
Alexsnder <krut.pitt@yandex.ru>
2024-08-22 19:45:09 +0300

    Sem9

---
+++ data.js
@@ -0,0 +1,26 @@
+let dataProductsJSON = `[
+    {
+        "image" : "1.png",
+        "title" : "MANGO  PEOPLE  T-SHIRT",
+        "price" : "$300",
+        "color" : "Red",
+        "size" : "Xl",
+        "quantity" : "2"
+    },
+    {
+        "image" : "2.png",
+        "title" : "ELLERY X M'O CAPSULE",
+        "price" : "$52",
+        "color" : "Black",
+        "size" : "Xl",
+        "quantity" : "1"
+    },
+    {
+        "image" : "3.png",
+        "title" : "NICE MAN CLOTHING",
+        "price" : "$502",
+        "color" : "Nuded",
+        "size" : "Xl",
+        "quantity" : "3"
+    }
+]`;
